8b8cf5bda61093aebfe01e5eda05ace03a13ccb3
Victor Huang <victor.huang@carta.com>
2023-11-17 10:55:45 -0800

    add a build_a_christmas_tree_exercise

---
+++ algorithms/recursion/build_a_christmas_tree_exercise.py
@@ -0,0 +1,23 @@
+"""
+we want to using recursion to print a christmas tree on the screen, here an example output of a tree with 5 level:
+
+    *
+   ***
+  *****
+ *******
+*********
+
+Expansion:
+once you print the desired three, you can decorate it with the printing of the top of the tree with a Red color star
+"""
+
+
+def printTreeLevel(level, totalLevels):
+    # base case / stopping case
+
+    # call itself again to solve for it "simpler" problem 
+    printTreeLevel()
+
+
+totalLevels = 10
+printTreeLevel(1, totalLevels)
